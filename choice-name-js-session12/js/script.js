8366fa126d615ac607b39a54830a7e30da864989
youssef <youssefmohamed2684@gmail.com>
2025-09-08 11:09:28 +0300

    choice-name-js-session12

---
+++ choice-name-js-session12/js/script.js
@@ -0,0 +1,8 @@
+var name = "youssef"
+for (var i = 1; i < 15; i++) {
+    var choice_name =prompt(`choice name (number ${i}) `)
+    if (name == choice_name){
+        alert(`you choice name in rule ${i}`)
+        break
+    } 
+}
