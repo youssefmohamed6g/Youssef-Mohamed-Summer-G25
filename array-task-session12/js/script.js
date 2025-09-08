27bdd6b1cf0fe39a7651b7a5e942a41d970b5d7a
youssef <youssefmohamed2684@gmail.com>
2025-09-08 14:23:11 +0300

    array-task-session12

---
+++ array-task-session12/js/script.js
@@ -0,0 +1,4 @@
+var course = prompt("enter the course")
+var courses = ["html", "css", "js", "bootstrap"];
+(courses.includes(course))? console.log("the course is found") 
+: console.log("the course is not found")
