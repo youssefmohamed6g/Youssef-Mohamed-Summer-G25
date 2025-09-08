0ee27a4cd47dbf19a32a677299be8e6e76801134
youssef <youssefmohamed2684@gmail.com>
2025-09-08 10:37:20 +0300

    degree-session12

--- degree-session12/js/script.js
+++ degree-session12/js/script.js
@@ -1,9 +1,12 @@
 var degree = prompt("please enter your degree")
-if(degree==null || degree=="" || typeof(degree)!=Number) console.log("plese enter your degree")
+if (degree == null || degree == "") console.log("plese enter your degree")
 else{
-    if(degree > 100) console.log("plese enter your corect degree")
-    else if(degree<=100 && degree>=90) console.log("you git a degree (A)")
-    else if(degree<90 && degree>=80) console.log("you git a degree (B)")
-    else if(degree<80 && degree>=70) console.log("you git a degree (C)")
-    else if(degree<70) console.log("you git a degree (F)")
+    if(degree > 100 || degree < 0) console.log("plese enter your correct degree")
+else if (degree <= 100 && degree >= 90) console.log("you git a degree (A)")
+else if (degree < 90 && degree >= 80) console.log("you git a degree (B)")
+else if (degree < 80 && degree >= 70) console.log("you git a degree (C)")
+else if (degree < 70 && degree >= 0) console.log("you git a degree (F)")
+else console.log("plese enter your correct degree")
 }
+
+
